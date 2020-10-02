8248394f956cda17c6cc91c465a5eef1ca2cd602
Matt Farmer <matt@frmr.me>
2020-10-02 15:37:36 -0400

    Make the thing runnable

--- core/src/main/scala/dispatch/HttpExecutor.scala
+++ core/src/main/scala/dispatch/HttpExecutor.scala
@@ -1,8 +1,10 @@
 package dispatch
 
+import java.io.InputStream
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
@@ -10,9 +12,13 @@ trait HttpExecutor {
   self =>
   def client: HttpClient
 
-  // def apply(req: Req)(implicit executor: ExecutionContext): Future[Response] = {
-  //   apply(req.toRequest -> new FunctionHandler(identity))
-  // }
+  def apply(req: Req)(implicit executor: ExecutionContext): Future[HttpResponse[InputStream]] = {
+     apply(req, BodyHandlers.ofInputStream())
+  }
+
+  def apply[T](req: Req, handler: HttpResponse.BodyHandler[T])(implicit executor: ExecutionContext): Future[HttpResponse[T]] = {
+    apply(req.toRequest, handler)
+  }
 
   def apply[T](pair: (HttpRequest, HttpResponse.BodyHandler[T]))
               (implicit executor: ExecutionContext): Future[T] = {
